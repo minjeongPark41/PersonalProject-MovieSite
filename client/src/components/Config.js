7aa73e4b41d041d58b5fd0729b8e136df9f0468c
minjeongPark41 <pmjabcd@gmail.com>
2021-11-18 17:06:33 +0900

    movieDB API 다운, 공통된 URL 주소를 Config.js에서 export

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+// 다른 곳에서도 이 API를 사용할 수 있도록 '공통된 주소를' export
+export const API_URL = 'http://api.themoviedb.org/3/';
+
+// 이미지 부분의 API도
+export const IMAGE_BASE_URL = 'http://image/tmdb.org/t/p'
