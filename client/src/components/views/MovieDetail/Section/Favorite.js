89a55c01da5a9868c5ef9bbe01da0cfb81bba577
minjeongPark41 <pmjabcd@gmail.com>
2021-12-04 23:35:44 +0900

    onClickFavorite 함수 로직 추가

--- client/src/components/views/MovieDetail/Section/Favorite.js
+++ client/src/components/views/MovieDetail/Section/Favorite.js
@@ -13,16 +13,14 @@ function Favorite(props) {
     const [FavoriteNumber, setFavoriteNumber] = useState(0)
     const [Favorited, setFavorited] = useState(false)
 
-
+    let variable ={
+        // 누가 좋아요 눌렀는지, 어떤 영화 좋아했는지에 대한 정보를 Axios에 같이 보내줘야 올바른 정보를 받을 수 있음
+        userFrom,
+        movieId
+    }
 
     // Favorite 누르면 해주고 싶은 것들 로직 짜주기
     useEffect(() => {
-
-        let variable ={
-            // 누가 좋아요 눌렀는지, 어떤 영화 좋아했는지에 대한 정보를 Axios에 같이 보내줘야 올바른 정보를 받을 수 있음
-            userFrom,
-            movieId
-        }
 
         // fetch, axios 다 사용 가능. (우선 get, post 나누지 말고 다 post로 만들어보자)
         // endpoint는 내가 임의로 정해줄 수 있음
@@ -56,12 +54,40 @@ function Favorite(props) {
 
     }, [])
 
+    const onClickFavorite = () => {
+
+        if(Favorited){
+            // Favorited 있으면 삭제해주기 
+            Axios.post('/api/favorite/removeFromFavorite', variable)
+                .then(response => {
+                    if (response.data.success){
+
+                    }else{
+                        alert('Favorite 삭제를 실패했습니다.')
+                    }
+                })
+
+        }else{
+            // Favorited 없으면 추가해주기
+            Axios.post('/api/favorite/addFavorite', variable)
+                .then(response => {
+                    if (response.data.success){
+
+                    }else{
+                        alert('Favorite 추가를 실패했습니다.')
+                    }
+                })
+
+        }
+
+    }
+
 
     return (
         <div>
             {/* Favotied가 true면 - 즉, 좋아요가 있었으면 */}
             {/* {FavoriteNumber는 현재 좋아요 '숫자' 보여주는거} */}
-            <button>{Favorited? "Cancle Favorite": "Add to Favorite"} {FavoriteNumber} </button>
+            <button onClick={onClickFavorite}>{Favorited? "Cancle Favorite": "Add to Favorite"} {FavoriteNumber} </button>
         </div>
     )
 }
